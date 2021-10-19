b6aa2dbcf4e1408f03c5d9264804265258dc35ed
Charlie Strawn <cdstrawn@gmail.com>
2021-10-19 11:53:57 -0500

    fix todos, filter lib versions

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for frugal.
 GH_REPO="https://github.com/Workiva/frugal"
 TOOL_NAME="frugal"
 TOOL_TEST="frugal --version"
@@ -27,12 +26,12 @@ sort_versions() {
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
+    # exclude releases of the library
+    grep -v 'lib/' |
     sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if frugal has other means of determining installable versions.
   list_github_tags
 }
 
@@ -41,7 +40,6 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for frugal
   url="$GH_REPO/archive/v${version}.tar.gz"
 
   echo "* Downloading $TOOL_NAME release $version..."
